e9614b4e92254ae3f526af31e95a992524bdb74c
kunrich <kunrich202202@gmail.com>
2021-01-25 19:49:07 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -11,40 +11,8 @@ if(req.query.url){
 var str1 = Buffer.from(req.query.url, 'base64').toString('utf-8');
 
 request(str1, function (error, response, body) {
-	if (!error && response.statusCode == 200) {
-		console.log("ok request 1...");
-		var a1=body.substring(body.search("https://player.marimo.life/"),body.search('" scrolling='));
-		if(a1!=""){
-		
-request(a1, function (error, response, body) {
-	if (!error && response.statusCode == 200) {
-		console.log("ok request 2...");
-		var t1='<div id="player"><script>contents';
-		var t2=';</script></div>';
-		var b1=body.substring(body.search(t1),body.search(t2)).substr(t1.length+2);
-		var b2=b1.substring(0,b1.length-2);
-		var b3=b2.split("','");
-		
-request.post({
-url: 'https://player.marimo.life/demo/s/'+b3[1]+'/',
-body: 'v='+b3[0]+'&r='+b3[2],
-headers: {
-'Referer': a1,
-'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
-}
-}, function(error, response, body){
 res.send(body);
-});
-
-
-	}else{res.send("error");}
-})	
-
-		}
-	}else{res.send("error");}
 })
-
-
 
 }else{
 res.send("error");
