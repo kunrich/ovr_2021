d850e40dfe6c8e434991e9e17708ab27b58b90dd
kunrich <kunrich202202@gmail.com>
2021-01-25 23:44:34 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
-var express = require("express");
-var app     = express();
-var port    = process.env.PORT || 80;
-var request = require("request");
+var http     = require('http');
+var request  = require('request');
+var url='https://player.marimo.life/demo/?key=19qk0qWgZpKSoMujx2WfxK-WzG7Q0N-Y&vid=18fjveikp2HO6Kl5w82v1MjX25vSwtx869Pcl8afmYrV0KvSz6-qsaXJpLHusM-fieKcjNewjaaR2OOutcHBhurC3qLDomOxttWa58ir0t3emqOQytm5manmh4Cuwqyuuuzgr6TA5q_Gtb-HmsGExKWRs97G7J_RwZi7t7rRvIKeuoiNr6il1tXVr6TVm-KfyeLImtDbnH2wkXmgrLjQuJ-Z1Li51L-frdOfo8CUoteq';
 
 
-app.use(express.static('public'));
-app.get("/",(req,res)=>{
-
-if(req.query.url){
-var str1 = Buffer.from(req.query.url, 'base64').toString('utf-8');
-// res.send(body);
-
-request(str1, function (error, response, body) {
+http.createServer(function (req, res){
+	console.log("ok goto get");
+	request(url, function (error, response, body) {
 	if (!error && response.statusCode == 200) {
-		console.log("ok request 1...");
+		console.log("inget 1");
 		var a1=body.substring(body.search("https://player.marimo.life/"),body.search('" scrolling='));
-		if(a1!=""){
+if(a1!=""){
+	url=a1;
+}
 		
-request(a1, function (error, response, body) {
+request(url, function (error, response, body) {
+	console.log("inget 2");
 	if (!error && response.statusCode == 200) {
-		console.log("ok request 2...");
 		var t1='<div id="player"><script>contents';
 		var t2=';</script></div>';
 		var b1=body.substring(body.search(t1),body.search(t2)).substr(t1.length+2);
@@ -34,21 +30,26 @@ headers: {
 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
 }
 }, function(error, response, body){
-res.send(body);
+console.log("inget 3");
+res.write(body);
+res.end();
 });
 
 
-	}else{res.send("error");}
-})	
-
-		}
-	}else{res.send("error");}
+}else{console.log("error pass 2");}
 });
 
-}else{
-res.send("error");
-}
-
+}else{console.log("error pass1");}
 });
 
-app.listen(port);
+}).listen(8080); 
+
+
+console.log("ok file");
+
+
+
+
+
+
+
