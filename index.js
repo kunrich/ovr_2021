739249dd84a17ce4fddb86dd3e31f8b286f357fe
kunrich <kunrich202202@gmail.com>
2021-01-25 19:38:53 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-var express = require('express');
-var request = require('request');
+var express=require("express");
+var app=express();
+var port=process.env.PORT || 80;
 
-var app = express();
-app.use(express.json());
+app.use(express.static('public'));
 
+app.get("/",(req,res)=>{
 
-app.get('/', (req, res) => {
 if(req.query.url){
 var str1 = Buffer.from(req.query.url, 'base64').toString('utf-8');
 res.send(str1);
 console.log(str1);
 }else{
- res.send("none");
+res.send("none");
 }
-console.log("END");
+
 });
 
-
-var port = process.env.PORT || 80;
-app.listen(port, () => console.log(`Listening on port${port}...`) );
+app.listen(port);
