c696df07b9b0f8f7f4089534d2f7da846f7de8a0
kunrich <kunrich202202@gmail.com>
2021-01-26 00:08:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ var http     = require('http');
 
 http.createServer(function (req, res){
 
-res.write(res);
+res.write("post 80");
 res.end();
 
 }).listen(80); 
+
+http.createServer(function (req, res){
+
+res.write("post 8080");
+res.end();
+
+}).listen(8080); 
 
 
 
